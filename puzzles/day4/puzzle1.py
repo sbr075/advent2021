f22cb7fb41ebebb1f4aa2a545095d0a632be03fc
sbr075 <sbrynjulf@hotmail.com>
2021-12-04 20:06:07 +0100

    even even shorter

--- puzzles/day4/puzzle1.py
+++ puzzles/day4/puzzle1.py
@@ -1,50 +1,37 @@
 import numpy as np
 
-order_of_numbers = [83,5,71,61,88,55,95,6,0,97,20,16,27,7,79,25,81,29,22,52,43,21,53,59,99,18,35,96,51,93,14,77,15,3,57,28,58,17,50,32,74,63,76,84,65,9,62,67,48,12,8,68,31,19,36,85,98,30,91,89,66,80,75,47,4,23,60,70,87,90,13,38,56,34,46,24,41,92,37,49,73,10,94,26,42,40,33,54,86,82,72,39,2,45,78,11,1,44,69,64]
+numbers = [83,5,71,61,88,55,95,6,0,97,20,16,27,7,79,25,81,29,22,52,43,21,53,59,99,18,35,96,51,93,14,77,15,3,57,28,58,17,50,32,74,63,76,84,65,9,62,67,48,12,8,68,31,19,36,85,98,30,91,89,66,80,75,47,4,23,60,70,87,90,13,38,56,34,46,24,41,92,37,49,73,10,94,26,42,40,33,54,86,82,72,39,2,45,78,11,1,44,69,64]
 
 def read_input():
     with open("input.txt", "r") as file:
         data = file.read().splitlines()
     
-    # Creates a list of lists with all numbers
-    # Removes empty strings because of double whitespace, and
-    # ignores empty lines
+    # Creates a list of matrixes (boards)
     data = [list(filter(None, line.split(" "))) for line in data if len(line) != 0]
     data = [data[n:n+5] for n in range(0, len(data), 5)]
-
     return data
+
+def update_boards(boards, number):
+    winning_board = None
+    for board in boards:
+        for i in range(len(board)):
+            board[i] = [n for n in board[i] if n != str(number)]
+
+            if len(board[i]) == 0:
+                winning_board = board
+                break
+    
+    return winning_board
 
 def main():
     data = read_input()
-
-    # Rows and cols
+    
     boards   = data
     boards_t = [np.array(matrix).T.tolist() for matrix in data]
+    for number in numbers:
+        for set_boards in [boards, boards_t]:
+            winning_board = update_boards(set_boards, number)
 
-    winning_board = None
-    winning_nr = None
-    for number in order_of_numbers:
-        for board in boards:
-            for i in range(len(board)):
-                board[i] = [n for n in board[i] if n != str(number)]
-        
-                if len(board[i]) == 0:
-                    winning_nr = number
-                    winning_board = board
-                    break
-        
-        if winning_board:
-            break
-
-        for board in boards_t:
-            for i in range(len(board)):
-                board[i] = [n for n in board[i] if n != str(number)]
-        
-                if len(board[i]) == 0:
-                    winning_nr = number
-                    winning_board = board
-                    break
-        
         if winning_board:
             break
 
@@ -52,8 +39,7 @@ def main():
     for line in winning_board:
         leftover += sum([int(n) for n in line])
 
-    print(leftover * winning_nr)
+    print(leftover * number)
         
-
 if __name__ == "__main__":
     main()
--- puzzles/day4/puzzle2.py
+++ puzzles/day4/puzzle2.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-order_of_numbers = [83,5,71,61,88,55,95,6,0,97,20,16,27,7,79,25,81,29,22,52,43,21,53,59,99,18,35,96,51,93,14,77,15,3,57,28,58,17,50,32,74,63,76,84,65,9,62,67,48,12,8,68,31,19,36,85,98,30,91,89,66,80,75,47,4,23,60,70,87,90,13,38,56,34,46,24,41,92,37,49,73,10,94,26,42,40,33,54,86,82,72,39,2,45,78,11,1,44,69,64]
+numbers = [83,5,71,61,88,55,95,6,0,97,20,16,27,7,79,25,81,29,22,52,43,21,53,59,99,18,35,96,51,93,14,77,15,3,57,28,58,17,50,32,74,63,76,84,65,9,62,67,48,12,8,68,31,19,36,85,98,30,91,89,66,80,75,47,4,23,60,70,87,90,13,38,56,34,46,24,41,92,37,49,73,10,94,26,42,40,33,54,86,82,72,39,2,45,78,11,1,44,69,64]
 
 def read_input():
     with open("input.txt", "r") as file:
@@ -22,12 +22,10 @@ def update_boards(boards, number, boards_left):
         for j in range(len(board)):
             board[j] = [n for n in board[j] if n != str(number)]
 
-            # If board has won
             if len(board[j]) == 0:
                 if len(boards_left) == 1:
                     losing_board = boards[boards_left[0]]
 
-                # Remove it from remaining boards
                 boards_left.remove(i)
                 break
     
@@ -39,27 +37,20 @@ def main():
     boards   = data
     boards_t = [np.array(matrix).T.tolist() for matrix in data]
     boards_left = [i for i in range(len(boards))]
-
-    losing_board = None
-    losing_nr = None
-    for number in order_of_numbers:
-        # Check horizontal
-        losing_board = update_boards(boards, number, boards_left)
-        if losing_board:
-            losing_nr = number
-            break
+    for number in numbers:
+        for set_boards in [boards, boards_t]:
+            losing_board = update_boards(set_boards, number, boards_left)
+            if losing_board:
+                break
         
-        # Check verticaø
-        losing_board = update_boards(boards_t, number, boards_left)
         if losing_board:
-            losing_nr = number
             break
 
     leftover = 0
     for line in losing_board:
         leftover += sum([int(n) for n in line])
 
-    print(leftover * losing_nr)
+    print(leftover * number)
         
 if __name__ == "__main__":
     main()
