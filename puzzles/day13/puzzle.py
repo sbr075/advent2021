e69698d9d455e4c9d1c686253af45237bfa1bae9
sbr075 <sbrynjulf@hotmail.com>
2021-12-14 01:37:01 +0100

    working version

--- puzzles/day13/puzzle.py
+++ puzzles/day13/puzzle.py
@@ -1,44 +1,50 @@
 import numpy as np
 
 def read_input():
+    points = []
+    folds  = []
     with open("input.txt", "r") as file:
-        data = file.read().splitlines()
-        data = [d.split(",") for d in data]
-        points = [p for p in data if len(p) == 2]
-        ins = [i for i in data if len(i) == 1 and len(i[0])!=0]
-    return points, ins
-
+        for l in file.read().splitlines():
+            if "fold" in l:
+                folds.append([l[11:12], int(l[13:])])
+            elif "," in l:
+                points.append(tuple([int(p) for p in l.split(",")])[::-1]) # reversed because numpy
+    return points, folds
 
 def main():
-    points, ins = read_input()
+    points, folds = read_input()
 
-    w = max([int(p[0]) for p in points])+1
-    h = max([int(p[1]) for p in points])+1
-
+    w = max([p[1] for p in points])+1
+    h = max([p[0] for p in points])+1
     m = np.zeros((h,w))
     for p in points:
-        m[int(p[1]),int(p[0])] = 1
-    
-    for i in range(len(ins)):
-        j=ins[i][0]
-        l = int("".join(filter(str.isdigit, j)))
-        if "x" in j:
-            l = (m.shape[1]-1) // 2
-            n_m = np.flip(m[:,l+1:], axis=1)
-            m = m[:,:l]
-        else:
-            l = (m.shape[0]-1) // 2
-            n_m = np.flip(m[l+2:,:], axis=0)
-            m = m[:l,:]
-        m = np.add(m, n_m)
+        m[p] = 1
 
+    for i in range(len(folds)):
+        axis = 0 if folds[i][0] == "y" else 1
+        line = folds[i][1]
+
+        if axis: # x
+            n = np.flip(m[:,line+1:], axis=axis)
+            m = m[:,:line]
+        else:    # y
+            n = np.flip(m[line+1:,:], axis=axis)
+            m = m[:line,:]
+        dy,dx = np.subtract(m.shape, n.shape)
+
+        for y in range(n.shape[0]):
+            for x in range(n.shape[1]):
+                m[y+dy,x+dx] += n[y,x]
+        
         if i == 0:
-            print(np.count_nonzero(m))
+            print(f"Part 1: {np.count_nonzero(m)}")
     
+    print("\nPart 2:")
     for r in m:
         for c in r:
             print("#" if c else " ", end=" ")
         print()
+    
     
 if __name__ == "__main__":
     main()
