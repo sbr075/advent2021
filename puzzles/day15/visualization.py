18d414063e476434b13351e6d4ce8acd30f7f345
sbr075 <sbrynjulf@hotmail.com>
2021-12-15 23:59:16 +0100

    updated visualization for day15

--- puzzles/day15/visualization.py
+++ puzzles/day15/visualization.py
@@ -4,20 +4,12 @@ Written fast just to see how the puzzle looks like visually
 
 import pygame
 import numpy as np
-from puzzle import read_input, get_verticies
 import time
+from random import randrange
 
-MAIN_TEXT  = (32, 170, 24)
+from puzzle import read_input, get_verticies, ext_grid
 
-LOCKED     = (32, 50, 24)
-UNLOCKED   = (32, 130, 24)
-SEARCHING  = (32, 220, 24)
-
-BACKGROUND = (10, 30, 55)
-
-START = (24, 97, 171)
-END   = (171, 31, 24)
-
+# SETTINGS
 SCREEN_X = 800
 SCREEN_Y = 800
 RESOLUTION = (SCREEN_X, SCREEN_Y)
@@ -28,76 +20,98 @@ display = pygame.display.set_mode(RESOLUTION)
 
 MAIN_FONT = pygame.font.SysFont("consolas", 20, bold=True)
 
-CLOCK = 5
-
 clock = pygame.time.Clock()
+
+# COLORS
+START = (0, 174, 255)
+END   = (171, 31, 24)
+BACKGROUND = (156, 156, 156)
+LINE = (50, 50, 50)
+
+class Rect():
+    def __init__(self, GRID, START, END):
+        self.GRID = GRID
+        self.RECT_H = SCREEN_Y / GRID.shape[1]
+        self.RECT_W = SCREEN_X / GRID.shape[0]
+        self.START = START
+        self.END = END
+
+        self.POS = []
+        for y in range(self.GRID.shape[1]):
+            for x in range(self.GRID.shape[0]):
+                y_loc = (SCREEN_Y / self.GRID.shape[1]) * y
+                x_loc = (SCREEN_X / self.GRID.shape[0]) * x
+                self.POS.append((y_loc, x_loc))
+    
+    def draw(self, y, x, c, w):
+        y_loc, x_loc = self.POS[int(y*self.GRID.shape[1] + x)]
+        r = pygame.Rect(x_loc, y_loc, self.RECT_W, self.RECT_H)
+        pygame.draw.rect(display, c, r, w)
+    
+    def init_draw(self):
+        for y in range(self.GRID.shape[1]):
+            for x in range(self.GRID.shape[0]):
+                c = (23, 92 + randrange(-10,10), 45)
+                self.draw(y, x, c, 0)
+                self.draw(y, x, LINE, 1)
+
+    def update(self, v):
+        self.draw(self.START[0], self.START[1], START, 0)
+        for p in v:
+            c = (34, 133 + randrange(-10,10), 66)
+            self.draw(p[0], p[1], c, 0)
+        self.draw(self.END[0], self.END[1], END, 0)
+        
+class Djikstra():
+    def __init__(self, GRID, CLOCK, START, END):
+        self.GRID = GRID
+        self.END = END
+        self.STACK = [[0, [START]]]
+        self.FOUND = set([START])
+        self.CLOCK = CLOCK
+        self.RECT  = Rect(GRID, START, END)
+
+    def run(self):
+        self.RECT.init_draw()
+
+        while True:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    exit()
+            
+            c = self.STACK[0][0]
+            p = self.STACK[0][1]
+
+            vs = get_verticies(self.GRID, p[-1][0], p[-1][1], self.FOUND)
+            self.FOUND |= set(vs)
+
+            if self.END in self.FOUND:
+                for n in p:
+                    c = (0, randrange(150,220), randrange(174,255))
+                    self.RECT.draw(n[0], n[1], c, 0)
+                    pygame.display.update()
+                    clock.tick(int(self.CLOCK/2))
+                time.sleep(5)
+                exit()
+
+            self.RECT.update(vs)
+            pygame.display.update()
+
+            del self.STACK[0]
+            self.STACK += [[c+self.GRID[pr], p+[pr]] for pr in vs]
+            self.STACK = sorted(self.STACK, key=lambda x: x[0])
+
+            clock.tick(self.CLOCK)
+
 def main():
     grid = np.array(read_input())
-    e = tuple(np.subtract(grid.shape, (1,1)))
+    grid = ext_grid(grid)
 
-    stack = [[0, [(0,0)]]]
-    v = set([(0,0)])
-    searched = []
-
-    display.fill(BACKGROUND)
-    while True:
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                exit()
-        
-        c = stack[0][0]
-        p = stack[0][1]
-        searched.append(p[-1])
-
-        if p[-1] == e:
-            shortest_txt = MAIN_FONT.render(f"Shortest path: {stack[0][0]}", True, MAIN_TEXT)
-            display.blit(shortest_txt, (30, 30))
-
-            for p in stack[0][1]:
-                y = p[1]
-                x = p[0]
-                r = pygame.Rect(40+((SCREEN_Y - 20) / 10)*y, 80+((SCREEN_X - 60) / 10)*x, 20, 20)
-                pygame.draw.rect(display, START, r)
-                pygame.display.update()
-                clock.tick(CLOCK)
-
-            time.sleep(5)
-            break
-        
-        vs = get_verticies(grid, p[-1][0], p[-1][1], v)
-        v|=set(vs)
-
-        del stack[0]
-        stack += [[c+grid[pr], p+[pr]] for pr in vs]
-
-        stack = sorted(stack, key=lambda x: x[0])
-
-        rects = []
-        for y in range(grid.shape[0]):
-            for x in range(grid.shape[1]):
-                r = pygame.Rect(40+((SCREEN_Y - 20) / 10)*y, 80+((SCREEN_X - 60) / 10)*x, 20, 20)
-                if (y,x) == (0,0):
-                    color = START
-                elif (y,x) == e:
-                    color = END
-                elif (y,x) == p[-1]:
-                    color = SEARCHING
-                elif (y,x) in searched:
-                    color = UNLOCKED
-                else:
-                    color = LOCKED
-                rects.append([color, r])
-        
-        display.fill(BACKGROUND, rect=(0,0, SCREEN_X, 70))
-        searched_txt = MAIN_FONT.render(f"Searched: {len(searched)}", True, MAIN_TEXT)
-        display.blit(searched_txt, (30, 10))
-
-        for rect in rects:
-            pygame.draw.rect(display, rect[0], rect[1])
-
-        pygame.display.update()
-        clock.tick(CLOCK)
+    start = (0,0)
+    end   = tuple(np.subtract(grid.shape, (1,1)))
+    prog = Djikstra(grid, 60, start, end)
+    prog.run()
 
 if __name__ == "__main__":
     main()
