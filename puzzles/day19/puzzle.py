20c9d7876b24e23ce6e5ecc0c3a218fb551a73b5
sbr075 <sbrynjulf@hotmail.com>
2021-12-19 22:40:17 +0100

    day19 optimizations

--- puzzles/day19/puzzle.py
+++ puzzles/day19/puzzle.py
@@ -1,11 +1,46 @@
 import numpy as np
 
-class Scanner():
+class Matrix():
+    def rotate_x(self):
+        rot_mat = np.array([[1,0,0],[0,0,-1],[0,1,0]])
+        for i in range(len(self.beacons)):
+            self.beacons[i] = rot_mat.dot(self.beacons[i])
+
+    def rotate_y(self):
+        rot_mat = np.array([[0,0,1],[0,1,0],[-1,0,0]])
+        for i in range(len(self.beacons)):
+            self.beacons[i] = rot_mat.dot(self.beacons[i])
+
+    def rotate_z(self):
+        rot_mat = np.array([[0,-1,0],[1,0,0],[0,0,1]])
+        for i in range(len(self.beacons)):
+            self.beacons[i] = rot_mat.dot(self.beacons[i])
+    
+    def calc_dists(self):
+        dists = {}
+        for i in range(len(self.beacons)):
+            for j in range(i+1, len(self.beacons)):
+                d = np.linalg.norm(self.beacons[i]-self.beacons[j])
+                dists[d] = [self.beacons[i], self.beacons[j]]
+        return dists
+
+class Scanner(Matrix):
     def __init__(self, id):
         self.id = id
         self.pos = (0,0,0)
+        self.rotation = 0
+        self.data = {}
+        self.dists = None
         self.beacons = []
-        self.dists = {}
+
+    def update_data(self):
+        for i in range(64):
+            if i != 0:
+                self.rotate_z()
+                if i % 4 == 0: self.rotate_y()
+                if i % 16 == 0: self.rotate_x()
+
+            self.data[i] = [self.calc_dists(), self.beacons.copy()]
 
 def read_input():
     with open("input.txt", "r") as file:
@@ -21,13 +56,6 @@ def read_input():
 
 def updt(d, k, v):
     d[k] = v if k not in d else d[k] + v
-
-def calc_dists(s):
-    s.dists = {}
-    for i in range(len(s.beacons)):
-        for j in range(i+1, len(s.beacons)):
-            d = np.linalg.norm(s.beacons[i]-s.beacons[j])
-            updt(s.dists, d, [s.beacons[i], s.beacons[j]])
 
 def in_list(l, v):
     for i in l:
@@ -61,59 +89,40 @@ def shift_coords(s):
     for i in range(len(s.beacons)):
         s.beacons[i] = np.add(s.beacons[i], s.pos)
 
-def rotate_x(s):
-    rot_mat = np.array([[1,0,0],[0,0,-1],[0,1,0]])
-    for i in range(len(s.beacons)):
-        s.beacons[i] = rot_mat.dot(s.beacons[i])
-
-def rotate_y(s):
-    rot_mat = np.array([[0,0,1],[0,1,0],[-1,0,0]])
-    for i in range(len(s.beacons)):
-        s.beacons[i] = rot_mat.dot(s.beacons[i])
-
-def rotate_z(s):
-    rot_mat = np.array([[0,-1,0],[1,0,0],[0,0,1]])
-    for i in range(len(s.beacons)):
-        s.beacons[i] = rot_mat.dot(s.beacons[i])
-
 def main():
     scanners = read_input()
+    for s in scanners:
+        s.update_data()
 
     found = [scanners[0]]
     queue = [scanners[0]]
     while queue:
         s1 = queue.pop()
-        calc_dists(s1)
+        s1.dists = s1.data[s1.rotation][0]
 
         for s2 in scanners:
             if s1 == s2 or s2 in found:
                 continue
-
-            is_found = False
-            for _ in range(4):
-                for _ in range(4):
-                    for _ in range(4):
-                        calc_dists(s2)
-                        pairs = find_pairs(s1, s2)
-                        if not pairs:
-                            continue
-
-                        k = [tuple(np.subtract(p[0], p[1])) for p in pairs]
-                        if len(set(k)) == 1:
-                            s2.pos = tuple(np.add(k[0], s1.pos))
-                            queue.append(s2)
-                            found.append(s2)
-                            is_found = True
-                    
-                        if is_found: break
-                        rotate_z(s2)
-                    if is_found: break
-                    rotate_y(s2)
-                if is_found: break
-                rotate_x(s2)
-    
+            
+            for _ in range(64):
+                s2.dists = s2.data[s2.rotation][0]
+                pairs = find_pairs(s1, s2)
+                if not pairs:
+                    continue
+                
+                k = [tuple(np.subtract(p[0], p[1])) for p in pairs]
+                if len(set(k)) == 1:
+                    s2.pos = tuple(np.add(k[0], s1.pos))
+                    queue.append(s2)
+                    found.append(s2)
+                    break
+                
+                s2.rotation += 1
+                s2.rotation %= 64
+           
     beacons = []
     for s in scanners:
+        s.beacons = s.data[s.rotation][1]
         shift_coords(s)
         beacons += [tuple(b) for b in s.beacons]
     print(f"Part 1 {len(set(beacons))}")
